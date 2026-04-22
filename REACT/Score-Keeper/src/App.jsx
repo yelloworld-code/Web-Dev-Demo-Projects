a2bc44cda8fc8c95904c09692f492908f17e56be
yelloworld-design <ankitdabas1999@gmail.com>
2026-04-22 16:18:24 +0530

    feat: initialize Score Keeper app

---
+++ REACT/Score-Keeper/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ScoreKeeper from "./ScoreKeeper";
+
+function App() {
+  return (
+    <div>
+      <ScoreKeeper numPlayers={5} target={5} />
+    </div>
+  );
+}
+
+export default App;
