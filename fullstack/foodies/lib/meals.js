2700f78d6a4788bd4f58b6147c686f7ac48bd0cd
yelloworld-design <ankitdabas1999@gmail.com>
2026-05-06 17:02:12 +0530

    style: prettier format

--- fullstack/foodies/lib/meals.js
+++ fullstack/foodies/lib/meals.js
@@ -1,11 +1,11 @@
-import sql from 'better-sqlite3';
+import sql from "better-sqlite3";
 
-const db = sql('meals.db'); // This will create the database file if it doesn't exist
+const db = sql("meals.db"); // This will create the database file if it doesn't exist
 
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay of 2 seconds to mimic a real database query
-  return db.prepare('SELECT * FROM meals').all(); 
-  //.all() returns an array of all rows in the meals table
-  //.get() would return only the first row, and .run() is used for insert/update/delete operations
-  //.run() returns an object with information about the operation, but not the data itself
+	await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay of 2 seconds to mimic a real database query
+	return db.prepare("SELECT * FROM meals").all();
+	//.all() returns an array of all rows in the meals table
+	//.get() would return only the first row, and .run() is used for insert/update/delete operations
+	//.run() returns an object with information about the operation, but not the data itself
 }
