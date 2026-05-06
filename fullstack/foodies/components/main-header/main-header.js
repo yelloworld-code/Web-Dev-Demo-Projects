4fbebf5123950d7da2854abc1b12e66db1cbec6a
yelloworld-design <ankitdabas1999@gmail.com>
2026-05-06 16:36:29 +0530

    feat: refactor MainHeader component; add NavLink for navigation and improve styles

--- fullstack/foodies/components/main-header/main-header.js
+++ fullstack/foodies/components/main-header/main-header.js
@@ -1,24 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import MainHeaderBackground from "./main-header-background";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
-import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
 	return (
 		<>
 			<MainHeaderBackground />
 			<header className={classes.header}>
-				<Link href="/" className={classes.logo}>
-					<Image src={logoImg} alt="NextLevel Food" priority />
+				<Link className={classes.logo} href="/">
+					<Image src={logoImg} alt="A plate with food on it" priority />
 					NextLevel Food
 				</Link>
 
 				<nav className={classes.nav}>
 					<ul>
-						<Link href="/meals">Browse Meals</Link>
-						<Link href="/community">Foodies Community</Link>
+						<li>
+							<NavLink href="/meals">Browse Meals</NavLink>
+						</li>
+						<li>
+							<NavLink href="/community">Foodies Community</NavLink>
+						</li>
 					</ul>
 				</nav>
 			</header>
