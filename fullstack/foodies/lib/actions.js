d34f89fe6c62afdab25d0c09129f021e4b5c9e6c
yelloworld-design <ankitdabas1999@gmail.com>
2026-05-07 20:18:59 +0530

    feat(ShareMealPage): integrate MealsFormSubmit component and add error handling

--- fullstack/foodies/lib/actions.js
+++ fullstack/foodies/lib/actions.js
@@ -1,18 +1,36 @@
-'use server';
+"use server";
+
+import { redirect } from "next/navigation";
 
 import { saveMeal } from "./meals";
-import { redirect } from "next/navigation"; // A function from Next.js that allows you to programmatically redirect the user to a different page after an action is completed (e.g., after sharing a meal, the user is redirected to the meals listing page)
+
+function isInvalidText(text) {
+	return !text || text.trim() === "";
+}
 
 export async function shareMeal(formData) {
-  const meal = {
-    title: formData.get('title'),
-    summary: formData.get('summary'),
-    instructions: formData.get('instructions'),
-    image: formData.get('image'),
-    creator: formData.get('name'),
-    creator_email: formData.get('email'),
-  };
+	const meal = {
+		title: formData.get("title"),
+		summary: formData.get("summary"),
+		instructions: formData.get("instructions"),
+		image: formData.get("image"),
+		creator: formData.get("name"),
+		creator_email: formData.get("email"),
+	};
 
-  await saveMeal(meal);
-  redirect("/meals");
+	if (
+		isInvalidText(meal.title) ||
+		isInvalidText(meal.summary) ||
+		isInvalidText(meal.instructions) ||
+		isInvalidText(meal.creator) ||
+		isInvalidText(meal.creator_email) ||
+		!meal.creator_email.includes("@") ||
+		!meal.image ||
+		meal.image.size === 0
+	) {
+		throw new Error("Invalid input");
+	}
+
+	await saveMeal(meal);
+	redirect("/meals");
 }
