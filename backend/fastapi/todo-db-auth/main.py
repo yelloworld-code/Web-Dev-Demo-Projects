f7c5a02dd9cb1e4faf2e857190f09dadf788aae2
yelloworld-design <ankitdabas1999@gmail.com>
2026-05-12 18:40:25 +0530

    feat: implement CRUD operations for Todos and add database session management

--- backend/fastapi/todo-db-auth/main.py
+++ backend/fastapi/todo-db-auth/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
+from pydantic import BaseModel, Field
+
 import models
-from database import engine
+from models import Todos
+from database import SessionLocal, engine
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -8,8 +14,90 @@ models.Base.metadata.create_all(bind=engine)
 # This line creates all the tables defined in your SQLAlchemy models in the database. 
 # The create_all method is called on the metadata of the Base class, which is the base class for all your models. 
 # The bind parameter specifies the database engine to use for creating the tables. 
-# In this case, it uses the engine that was created in the database.py file, which is connected to the SQLite database specified in the SQLALCHEMY_DATABASE_URL variable.
+# In this case, it uses the engine that was created in the database.py file, 
+# which is connected to the SQLite database specified in the SQLALCHEMY_DATABASE_URL variable.
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    # In this context, the finally block ensures that the database session is closed after it has been used, 
+    # regardless of whether an exception occurred or not. 
+    # The get_db function is a generator function that yields a database session. 
+    # When the function is called, it creates a new session and yields it to the caller. 
+    # After the caller is done using the session, the finally block is executed, 
+    # which closes the session to free up resources and prevent potential memory leaks.
+
+db_dependency = Annotated[Session, Depends(get_db)]
+    # db:db_dependency is a parameter that indicates that the db variable is of type Session 
+    # and is obtained from the get_db function using FastAPI's dependency injection system.
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3, max_length=100)
+    description: str = Field(min_length=3, max_length=200)
+    priority: int = Field(ge=1, le=5)
+    completed: bool = False
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"} 
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()  
+
+
+
+@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail={"error": "Todo not found"})
+
+
+
+@app.post("/todos/", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo: TodoRequest):
+    todo_model = Todos(
+        title=todo.title,
+        description=todo.description,
+        priority=todo.priority,
+        completed=todo.completed
+    )
+    db.add(todo_model)
+    db.commit()
+    return todo_model
+
+
+
+@app.put("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, 
+                        todo: TodoRequest = None ,
+                        todo_id: int = Path(gt=0)
+                    ):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail={"error": "Todo not found"})
+    
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.completed = todo.completed
+    
+    db.add(todo_model)
+    db.commit()
+    return None
+
+
+
+@app.delete("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail={"error": "Todo not found"})
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.commit()
+    return None
